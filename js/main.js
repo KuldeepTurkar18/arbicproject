4a6f4ececf267d28d778a999e95233e9b86f0585
unknown <priyansh@innoppl.com>
2022-06-17 11:32:52 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -23,3 +23,53 @@
 
 	  });
 	});
+
+$(document).ready(function() {
+
+  $('#serviceform').submit(function(e) {
+    e.preventDefault();
+    var first_Id = $('#id-num').val();
+    var code = $('#code').val();
+    var recode = $('#recode').val();
+
+    $(".error").remove();
+
+    if (first_Id.length < 1) {
+      $('#id-num').after('<span class="error text-danger">This field is required</span>');
+    }
+
+    if (code.length < 0) {
+      $('#code').after('<span class="error text-danger">This field is required</span>');
+    }
+
+    if (recode.length < 0) {
+      $('#recode').after('<span class="error text-danger">This field is required</span>');
+    }
+
+	if (code != recode) {
+		 $('#recode').after('<span class="error text-danger">**Password not match </span>');
+	      // recode = false;
+	      // return false;
+	 }
+
+	$('#loaderIcon').css('visibility', 'visible');       
+    $('#loaderIcon').show().delay(2000).fadeOut();
+
+  });
+	$("#code,#recode").keypress(function (e) {
+	   if (String.fromCharCode(e.keyCode).match(/[^0-9]/g)) return false;
+	});
+});
+
+$(document).ready(function(){
+  var height = [];
+
+  $('.our-work-section .card').each(function(){
+    height.push($(this).height());
+  });
+
+  var maxheight = Math.max.apply(null,height);
+
+  $('.our-work-section .card').height(maxheight);
+
+});
